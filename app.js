1f96bf1295a9ef48515c2badc7aa94c950a6c0de
chuluq <49191833+chuluq@users.noreply.github.com>
2020-12-18 17:08:40 +0700

    calculate number

--- app.js
+++ app.js
@@ -1,59 +1,82 @@
 // Listen event when key is pressed
 const calculator = document.querySelector('.calculator');
 const keys = document.querySelector('.calculator__keys');
+const display = document.querySelector('.calculator__display');
 
+// show number in display
 keys.addEventListener('click', (e) => {
   if (e.target.matches('button')) {
     // determine type of key is clicked
+    // using data-action
     const key = e.target;
     const action = key.dataset.action;
-
-    if (!action) {
-      console.log('number is clicked!');
-    }
-
-    if (
-      action === 'add' ||
-      action === 'subtract' ||
-      action === 'multiply' ||
-      action === 'divide'
-    ) {
-      console.log('operator key!');
-    }
-
-    if (action === 'decimal') {
-      console.log('decimal key!');
-    }
-
-    if (action === 'clear') {
-      console.log('clear key!');
-    }
-
-    if (action === 'calculate') {
-      console.log('equal key!');
-    }
-  }
-});
-
-// show number in display
-const display = document.querySelector('.calculator__display');
-
-keys.addEventListener('click', (e) => {
-  if (e.target.matches('button')) {
-    const key = e.target;
-    const action = key.dataset.action;
+    const previousKeyType = calculator.dataset.previousKeyType;
     const keyContent = key.textContent;
     const displayedNum = display.textContent;
 
-    // if calculator shows 0
-    // replace display with textContent of clicked key
+    // no action means no data-set
+    // number key
     if (!action) {
-      if (displayedNum === '0') {
+      // hits number key after an operator key
+      Array.from(key.parentNode.children).forEach((k) =>
+        k.classList.remove('is-depressed')
+      );
+
+      // if calculator shows 0
+      // replace display with textContent of clicked key
+      // update display after click operator key
+      if (displayedNum === '0' || previousKeyType === 'operator') {
         display.textContent = keyContent;
       } else {
         // if non-zero number append it
         display.textContent = displayedNum + keyContent;
       }
     }
+
+    // decimal key
+    if (action === 'decimal') {
+      display.textContent = displayedNum + '.';
+    }
+
+    // higlight operator key when click
+    if (
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+    ) {
+      key.classList.add('is-depressed');
+      // Add custom attribute
+      calculator.dataset.previousKeyType = 'operator';
+
+      // save first number
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
+    }
+
+    // calculate number
+    if (action === 'calculate') {
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+
+      display.textContent = calculate(firstValue, operator, secondValue);
+    }
   }
 });
+
+const calculate = (n1, operator, n2) => {
+  let result = '';
+
+  if (operator === 'add') {
+    result = parseFloat(n1) + parseFloat(n2);
+  } else if (operator === 'subtract') {
+    result = parseFloat(n1) - parseFloat(n2);
+  } else if (operator === 'multiply') {
+    result = parseFloat(n1) * parseFloat(n2);
+  } else if (operator === 'divide') {
+    result = parseFloat(n1) / parseFloat(n2);
+  }
+
+  return result;
+};
